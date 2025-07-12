a93c82e22ffaaf3be7f5de1496c0949daf252070
tchilo <tchiloross@gmail.com>
2025-07-12 17:07:20 +0200

    Get input from the user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to the guessing game!");
+   println!("__________________________________") ;
+    println!("Guess a number from 1 to 100:");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Please use a number");
+    println!("{guess}");
 }

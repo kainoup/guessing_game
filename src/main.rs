df5833acbb4de23c464305304e380898380a90cd
tchilo <tchiloross@gmail.com>
2025-07-12 17:13:50 +0200

    Generate random secrete number with the rand crate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 
+use rand::Rng;
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Welcome to the guessing game!");
    println!("__________________________________") ;
     println!("Guess a number from 1 to 100:");
@@ -9,4 +11,5 @@ fn main() {
 
     io::stdin().read_line(&mut guess).expect("Please use a number");
     println!("{guess}");
+    println!("{secret_number}");
 }
